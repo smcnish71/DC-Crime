466b93d0d8414fcef8774f82398c3881ef65f7f8
smcnish71 <smcnish@deloitte.com>
2017-02-23 16:33:06 -0500

    added updates

--- DC Crime.R
+++ DC Crime.R
@@ -3,6 +3,8 @@ setwd("C:/Users/smcnish/Documents/Vizs/Crime/DC-Crime/raw data/")
 library(ggplot2)
 library(plyr)
 library(dplyr)
+library(extrafont)
+
 
 #append all 8 files together
 file_list <- list.files()
@@ -23,36 +25,53 @@ for (file in file_list){
   
 }
 
-
+#remove variables don't want
+delme <- c("SHIFT","START_DATE","END_DATE","CCN")
+dataset<- dataset[,-which(names(dataset) %in% delme)]
 
 #clean up variables
 
-dataset$hour = format(as.POSIXct(dataset$REPORT_DAT, format = "%m/%d/%Y %I:%M:%S %p"),"%I")
-dataset$day <- format(as.Date(dataset$REPORT_DAT,format="%Y-%m-%d %I:%M:%S %p"), "%d")        # day of month
-dataset$month <- format(as.Date(dataset$REPORT_DAT,format="%Y-%m-%d %I:%M:%S %p"), "%m")      # month of year 
-dataset$year <- format(as.Date(dataset$REPORT_DAT,format="%Y-%m-%d %I:%M:%S %p"), "%y")   # year
-dataset$START_DATE<- as.Date(dataset$START_DATE, format = "%m/%d/%Y %I:%M:%S %p")
-dataset$END_DATE<- as.Date(dataset$END_DATE, format = "%m/%d/%Y %I:%M:%S %p")
+dataset$REPORT_DAT <- strptime(dataset$REPORT_DAT, format = "%m/%d/%Y %I:%M:%S %p")
+
 dataset$date<- as.Date(dataset$REPORT_DAT, format = "%m/%d/%Y %I:%M:%S %p")
 dataset$DayofWeek <- weekdays(dataset$date)
 
-#filter out 2017
-dataset<-dataset[dataset$year != '17',]
+dataset$hour <- substr(dataset$REPORT_DAT,12,13)     #hour
+dataset$day <- substr(dataset$REPORT_DAT,9,10)    # day of month
+dataset$month <- substr(dataset$REPORT_DAT,6,7)      # month of year 
+dataset$year <- substr(dataset$REPORT_DAT,1,4)     # year
+
+
+#filter out 2017 because we only had a few dates
+dataset<-dataset[dataset$year != '2017',]
 
 #Create dataset of crime by day
-df_daily <- dataset %>%
+df_daily <- dataset[,-which(names(dataset) %in% c("REPORT_DAT"))] %>%
   group_by(date) %>% 
   summarize(count = n()) %>%
   arrange(date)
 
 #Plot crime by day
 ggplot(df_daily, aes(x = date, y = count)) +
-  geom_line(color = "#F2CA27", size = 0.1) +
+  geom_line(color = "#45b293", size = 0.1) +
   geom_smooth(color = "#1A1A1A") +
-  scale_x_date(date_breaks="2 years", date_labels = ("%Y")) +
-  labs(x = "Date of Crime", y = "# of Crimes", title = "Daily Crimes in DC from 2008 - 2016")
+  scale_x_date(date_breaks="1 year", date_labels = ("%Y")) +
+  labs(x = "Date of Crime", y = "# of Crimes", title = "Daily Crimes in DC from 2008 - 2016") +
+  theme(text=element_text(size = 16,family="Courier New" ))
 
 #summarize crime by day of the week and hour
-df_time <- dataset %>%
+df_time <- dataset[,-which(names(dataset) %in% c("REPORT_DAT"))] %>%
   group_by(DayofWeek, hour) %>%
   summarize(count = n())
+
+dow_format <- c("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+hour_format <- c(paste(c(12,1:11),"AM"), paste(c(12,1:11),"PM"))
+
+df_time$DayofWeek <- factor(df_time$DayofWeek, level = rev(dow_format))
+df_time$Hour <- factor(df_time$hour, level = 0:23, label = hour_format)
+
+ggplot(df_time, aes(x = hour, y = DayofWeek, fill = count)) +
+  geom_tile() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.6), legend.title = element_blank(), legend.position="top", legend.direction="horizontal", legend.key.width=unit(2, "cm"), legend.key.height=unit(0.25, "cm"), legend.margin=unit(-0.5,"cm"), panel.margin=element_blank()) +
+  labs(x = "Hour of Crime", y = "Day of Week of Crime", title = "# of Police Arrests in DC from 2008 - 2016, by Time of Arrest") +
+  scale_fill_gradient(low = "white", high = "#27AE60")
